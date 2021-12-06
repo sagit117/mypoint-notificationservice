fbd98cbe60f5e9eddb6fc66fec3f1b9f14a7cc78
Pavel Aksenov <sagit117@gmail.com>
2021-12-06 18:38:24 +0300

    корректировка mime message

--- src/ru/mypoint/notificationservice/connectors/Mailer.kt
+++ src/ru/mypoint/notificationservice/connectors/Mailer.kt
@@ -2,28 +2,47 @@ package ru.mypoint.notificationservice.connectors
 
 import org.apache.commons.mail.DefaultAuthenticator
 import org.apache.commons.mail.HtmlEmail
+import javax.mail.Message
 
 class Mailer(private val configMailer: ConfigMailer) {
-    fun send(subject: String, msgHtml: String, emails: Set<String>, altMsgText: String? = "Your email client does not support HTML messages") {
-        val email = HtmlEmail()
+    private val email = HtmlEmail()
 
+    init {
         email.hostName = configMailer.hostName
         email.setSmtpPort(configMailer.smtpPort)
         email.setAuthenticator(DefaultAuthenticator(configMailer.user, configMailer.password))
         email.isSSLOnConnect = configMailer.isSSLOnConnect
         email.setFrom(configMailer.from)
         email.setCharset(configMailer.charSet)
-        email.subject = subject
-        email.setMsg(msgHtml)
+    }
 
-        email.setHtmlMsg(msgHtml)
-        email.setTextMsg(altMsgText)
+    fun send(subject: String, msgHtml: String, emails: Set<String>, altMsgText: String? = "Your email client does not support HTML messages") {
+        if (email.mimeMessage == null) {
+            email.subject = subject
+//            email.setMsg(msgHtml)
+            email.setHtmlMsg(msgHtml)
+            email.setTextMsg(altMsgText)
 
-        for (mail in emails) {
-            email.addTo(mail)
+            for (mail in emails) {
+                email.addTo(mail)
+            }
+
+            email.buildMimeMessage()
+        } else {
+            email.mimeMessage.subject = subject
+            email.mimeMessage.setContent(msgHtml, "text/html; charset=utf-8")
+//            email.mimeMessage.setText(altMsgText)
+
+            email.mimeMessage.setRecipients(Message.RecipientType.TO, emptyArray())
+
+            for (mail in emails) {
+                email.mimeMessage.addRecipients(Message.RecipientType.TO, mail)
+            }
+
+            email.mimeMessage.saveChanges()
         }
 
-        email.send()
+        email.sendMimeMessage()
     }
 }
 
